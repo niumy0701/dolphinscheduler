c4599eb0de93a15f4d9c88fc429b5ddf317d3581
niumy <niumy@chinatelecom.cn>
2026-02-05 11:11:11 +0800

    [Improvement-17946][DIST] Optimize the dist assembly build

--- dolphinscheduler-dist/src/main/assembly/assembly-plugins.sh
+++ dolphinscheduler-dist/src/main/assembly/assembly-plugins.sh
@@ -19,15 +19,12 @@ set -xeo pipefail
 
 PLUGINS_ASSEMBLY_SKIP=$1
 
-DIST_DIR="$(pwd)/target"
-BIN_DIR="$DIST_DIR/apache-dolphinscheduler-*-bin"
-if [ ! -d $BIN_DIR ]; then
+DIST_DIR="$(readlink -f $(pwd)/target)"
+BIN_DIR=$(ls -d $DIST_DIR/apache-dolphinscheduler-*-bin | head -n1)
+if [ ! -d "$BIN_DIR" ]; then
   echo "$BIN_DIR not found!!!"
   exit 1
 fi
-
-cd $BIN_DIR
-BIN_DIR=$(pwd)
 
 # move *-plugins/target/*-plugin/target/*.jar to *-plugins/
 PLUGINS_PATH=(
@@ -40,11 +37,11 @@ task-plugins
 if [ $PLUGINS_ASSEMBLY_SKIP == "true" ]; then
   rm -rf $BIN_DIR/plugins/*
 else
-  for plugin_path in ${PLUGINS_PATH[@]}
-  do
-    cd $BIN_DIR/plugins/$plugin_path
-    find ./* -name "*.jar" | xargs -I {} mv {} ./
-    ls -d */ | xargs -I {} rm -rf {}
+  for plugin_path in "${PLUGINS_PATH[@]}"; do
+    PLUGIN_DIR="$BIN_DIR/plugins/$plugin_path"
+    [ ! -d "$PLUGIN_DIR" ] && continue
+    mv -f "$PLUGIN_DIR"/*/*/*.jar "$PLUGIN_DIR/"
+    rm -rf "$PLUGIN_DIR"/*/
   done
 fi
 
@@ -62,30 +59,27 @@ mkdir -p "$SHARED_LIB_DIR"
 
 for module in "${MODULES_PATH[@]}"; do
     MODULE_LIB_DIR="$BIN_DIR/$module/libs"
-    [ ! -d "$MODULE_LIB_DIR" ] && continue
+    [[ ! -d "$MODULE_LIB_DIR" ]] && continue
     cd "$MODULE_LIB_DIR" || continue
-
     local_jars=()
     for jar in *.jar; do
-        [ -f "$jar" ] && local_jars+=("$jar")
+        [[ -f "$jar" ]] && local_jars+=("$jar")
     done
-    [ ${#local_jars[@]} -eq 0 ] && continue
-
-    tar -cf - "${local_jars[@]}" | tar -xf - -C "$SHARED_LIB_DIR"
-
+    [[ ${#local_jars[@]} -eq 0 ]] && { cd - >/dev/null 2>&1; continue; }
+    cp -f "${local_jars[@]}" "$SHARED_LIB_DIR/"
     rm -f "${local_jars[@]}"
-
     for jar_name in "${local_jars[@]}"; do
-        ln -s ../../libs/"$jar_name" "$jar_name"
+        ln -sf ../../libs/"$jar_name" "$jar_name"
     done
+    cd - >/dev/null 2>&1
 done
 
 # create symbolic link for standalone-server
-cd $BIN_DIR/standalone-server && ln -s ../tools/sql/sql sql
+ln -sf "$BIN_DIR/tools/sql/sql" "$BIN_DIR/standalone-server/sql"
 
 # repack bin tar
 BIN_DIR_NAME=$(basename $BIN_DIR)
 BIN_TAR_FILE_NAME=$BIN_DIR_NAME.tar.gz
-cd $DIST_DIR && tar -zcf $BIN_TAR_FILE_NAME $BIN_DIR_NAME
+tar -zcf "$DIST_DIR/$BIN_TAR_FILE_NAME" -C "$DIST_DIR" "$BIN_DIR_NAME"
 
 echo "assembly-plugins.sh done"
